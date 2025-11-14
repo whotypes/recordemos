ecbd979f3f6038c7feb9d53ec77da46d7c5330db
whotypes <194645808+whotypes@users.noreply.github.com>
2025-11-13 20:01:07 -0800

    🚀 clean up landing page; lets cook

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,12 +6,10 @@ import {
 	ProPricingCards,
 } from "@/components/autumn/pricing-cards";
 import { HeroTitle } from "@/components/marketing/block";
-import { GitHub } from "@/components/svg-repo";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { createFileRoute, Link } from "@tanstack/react-router";
-import { Star } from "lucide-react";
 
 export const Route = createFileRoute("/")({
 	component: Home,
@@ -36,63 +34,6 @@ function Home() {
 				</Link>
 				<div className="flex items-center gap-4">
 					<ThemeToggle />
-					<div className="flex items-center gap-6">
-						<a
-							href="https://github.com/get-convex/convex-saas/tree/main/docs"
-							target="_blank"
-							rel="noreferrer"
-							className={cn(
-								buttonVariants({ variant: "link", size: "sm" }),
-								"group flex gap-3 px-0 text-primary/80 hover:text-primary hover:no-underline",
-							)}
-						>
-							Docs
-						</a>
-						<a
-							href="https://github.com/get-convex/convex-saas"
-							target="_blank"
-							rel="noreferrer"
-							className={cn(
-								buttonVariants({ variant: "link", size: "sm" }),
-								"group flex gap-3 px-0 text-primary/80 hover:text-primary hover:no-underline",
-							)}
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								className="h-5 w-5 text-primary/80"
-								viewBox="0 0 24 24"
-								fill="currentColor"
-							>
-								<path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-							</svg>
-							<span className="hidden select-none items-center gap-1 rounded-full bg-green-500/5 px-2 py-1 pr-2.5 text-xs font-medium tracking-tight text-green-600 ring-1 ring-inset ring-green-600/20 backdrop-blur-sm dark:bg-yellow-800/40 dark:text-yellow-100 dark:ring-yellow-200/50 md:flex">
-								<Star
-									className="h-3 w-3 text-green-600 dark:text-yellow-100"
-									fill="currentColor"
-								/>
-								Star Us on GitHub
-							</span>
-						</a>
-					</div>
-					<a
-						href="https://twitter.com/convex_dev"
-						target="_blank"
-						rel="noreferrer"
-						className="flex h-9 w-9 items-center justify-center"
-					>
-						<svg
-							className="h-[18px] w-[18px] text-primary"
-							strokeLinejoin="round"
-							viewBox="0 0 16 16"
-						>
-							<path
-								fillRule="evenodd"
-								clipRule="evenodd"
-								d="M0.5 0.5H5.75L9.48421 5.71053L14 0.5H16L10.3895 6.97368L16.5 15.5H11.25L7.51579 10.2895L3 15.5H1L6.61053 9.02632L0.5 0.5ZM12.0204 14L3.42043 2H4.97957L13.5796 14H12.0204Z"
-								fill="currentColor"
-							/>
-						</svg>
-					</a>
 					<Link to={"/studio"} className={buttonVariants({ size: "sm" })}>
 						Get Started
 					</Link>
@@ -106,16 +47,17 @@ function Home() {
 						href="https://github.com/whotypes/recordemos"
 						target="_blank"
 						rel="noreferrer"
-						className={cn(
-							buttonVariants({ size: "sm", variant: "outline" }),
-							"hidden md:flex",
-							"dark:bg-secondary dark:hover:opacity-80",
-						)}
+						className="hidden h-10 select-none items-center gap-2 rounded-full bg-green-500/5 px-2 py-1 pr-2.5 text-base font-medium tracking-tight text-green-600 ring-1 ring-inset ring-green-600/20 backdrop-blur-sm dark:bg-yellow-800/40 dark:text-yellow-100 dark:ring-yellow-200/50 md:flex"
 					>
-						Star{" "}
-						<Star className="w-4 h-4 mx-1 stroke-amber-300 fill-amber-300" />{" "}
-						Record Demos on{" "}
-						<GitHub className="size-4 ml-1.5 stroke-black stroke-width-2 fill-black" />
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-6 w-6 text-green-600 dark:text-yellow-100"
+							viewBox="0 0 24 24"
+							fill="currentColor"
+						>
+							<path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+						</svg>
+						Star Us on GitHub
 					</a>
 					<HeroTitle />
 					<div className="mt-2 flex w-full items-center justify-center gap-2">
@@ -126,7 +68,7 @@ function Home() {
 							Get Started
 						</Link>
 						<a
-							href="https://github.com/get-convex/convex-saas/tree/main/docs"
+							href="https://github.com/whotypes/recordemos"
 							target="_blank"
 							rel="noreferrer"
 							className={cn(
@@ -146,9 +88,9 @@ function Home() {
 						<a
 							target="_blank"
 							rel="noreferrer"
-							aria-label="Convex"
+							aria-label="Record Demos"
 							className="flex items-center text-primary opacity-80 grayscale transition hover:opacity-100"
-							href="https://convex.dev/c/remixsaastemplate"
+							href="https://recorddemos.com"
 						>
 							<div className="relative flex h-6 w-[148px] items-center justify-center">
 								<svg
@@ -207,7 +149,7 @@ function Home() {
 							rel="noreferrer"
 							aria-label="TanStack"
 							className="flex items-center text-primary opacity-80 grayscale transition hover:opacity-100"
-							href="https://tanstack.com/?utm_source=remixconvexsaas"
+							href="https://tanstack.com/?utm_source=recorddemos"
 						>
 							<svg
 								className="h-12 w-auto"
@@ -491,12 +433,12 @@ function Home() {
 						Proudly Open Source
 					</h1>
 					<p className="text-center text-lg text-primary/60">
-						Convex SaaS is a fully Open Source project.
+						Like Convex, Record Demos is a fully Open Source project.
 						<br />
 						The code is available on GitHub.
 					</p>
 					<a
-						href="https://github.com/get-convex/convex-saas"
+						href="https://github.com/whotypes/recordemos"
 						target="_blank"
 						rel="noreferrer"
 						className="hidden h-10 select-none items-center gap-2 rounded-full bg-green-500/5 px-2 py-1 pr-2.5 text-base font-medium tracking-tight text-green-600 ring-1 ring-inset ring-green-600/20 backdrop-blur-sm dark:bg-yellow-800/40 dark:text-yellow-100 dark:ring-yellow-200/50 md:flex"
@@ -517,7 +459,7 @@ function Home() {
 			{/* Footer */}
 			<footer className="z-10 flex w-full flex-col items-center justify-center gap-8 py-6">
 				<a
-					href="https://twitter.com/convex_dev"
+					href="https://twitter.com/notnyuma"
 					target="_blank"
 					rel="noreferrer"
 					className="flex items-center justify-center hover:scale-110"
@@ -535,37 +477,6 @@ function Home() {
 						/>
 					</svg>
 				</a>
-
-				<div className="flex flex-col items-center gap-2 sm:flex-row">
-					{/* Split the nested <p> so there is no <p> inside another <p> */}
-					<div className="flex items-center whitespace-nowrap text-center text-sm font-medium text-primary/60">
-						Based on&nbsp;
-						<a
-							href="https://remix-saas.fly.dev"
-							target="_blank"
-							rel="noreferrer"
-							className="flex items-center text-primary hover:text-primary hover:underline"
-						>
-							the Open-Source Remix SaaS by&nbsp;
-						</a>
-						<span>
-							<a href="https://bento.me/danielkanem?utm_source=remixconvexsaas">
-								DanielKanem.
-							</a>
-						</span>
-					</div>
-					<p className="flex items-center whitespace-nowrap text-center text-sm font-medium text-primary/60">
-						Source code available on&nbsp;{" "}
-						<a
-							href="https://github.com/get-convex/convex-saas"
-							target="_blank"
-							rel="noopener noreferrer"
-							className="flex items-center text-primary hover:text-primary hover:underline"
-						>
-							GitHub.
-						</a>
-					</p>
-				</div>
 			</footer>
 
 			{/* Background */}
